687c95c1cb8e30ebe851a2d3a51b355ef04bf598
Ujjwal Biswas <ujjwal117@gmail.com>
2020-09-10 01:37:05 +0530

    Added routes for pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,30 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:water_tap/screens/control.dart';
+import 'package:water_tap/screens/register_screen.dart';
+import 'package:water_tap/screens/welcome.dart';
 
 import 'screens/stream.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) => {runApp(VideoPlayerApp())});
+      .then((_) => {runApp(WaterTapApp())});
+}
+class WaterTapApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Player Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context)=> WelcomPage(),
+        '/register': (context) => RegisterPage(),
+        '/stream': (context) => VideoPlayerScreen(),
+        '/control': (context) => ControlCenter(),
+        
+      },
+    );
+  }
 }
